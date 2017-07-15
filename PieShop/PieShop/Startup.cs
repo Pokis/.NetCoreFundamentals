8e7015795f89f48abe3ccbc8f44b7fe0353c671c
Domantas Judeikis <djpokis@gmail.com>
2017-07-16 01:09:16 +0300

    Some initial pie models added. Registering services. Adding mock classes for temp data.

--- PieShop/PieShop/Startup.cs
+++ PieShop/PieShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PieShop.Models;
 
 namespace PieShop
 {
@@ -16,6 +17,9 @@ namespace PieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,10 +27,15 @@ namespace PieShop
         {
             loggerFactory.AddConsole();
 
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
