b2f1817dbdc688d0c92c30f4a740cd5165015333
Domantas Judeikis <djpokis@gmail.com>
2017-07-24 23:42:58 +0300

    Add orders model.

---
+++ PieShop/PieShop/Models/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    interface IOrderRepository
+    {
+        void CreateOrder(Order order);
+    }
+}
