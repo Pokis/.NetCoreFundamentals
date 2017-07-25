4cdd66ce042f35a97ca8e176234ae5cfbd51e6f4
Domantas Judeikis <djpokis@gmail.com>
2017-07-25 09:05:19 +0300

    Finishing adding order validation

--- PieShop/PieShop/Controllers/OrderController.cs
+++ PieShop/PieShop/Controllers/OrderController.cs
@@ -27,5 +27,31 @@ namespace PieShop.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if(_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "your cart is empty, add some pies first.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. See you soon.";
+            return View();
+        }
     }
 }
